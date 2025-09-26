25fb406d865311de55043281a2dfda332f1ccc8b
rathodvinod5 <rathodvinod5@gmail.com>
2025-09-26 17:26:23 +0530

    added struct for InitializePool

--- programs/stakely/src/instructions/initialize_pool.rs
+++ programs/stakely/src/instructions/initialize_pool.rs
@@ -1,4 +1,7 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token:: { Mint, Token };
+
+use crate::states::{ Pool };
 
 // Initialize the pool:
 // - Creates Pool account (PDA)
@@ -10,5 +13,41 @@ pub fn initialize_pool(ctx: Context<InitializePool>) -> Result<()> {
 
 #[derive(Accounts)]
 pub struct InitializePool<'info> {
-    pub authority: Signer<'info>
+    #[account(mut, signer)]
+    pub admin: AccountInfo<'info>, // AccountInfo indicates it can be wallet, PDA or DAO
+
+    // CHECK: mint created by client and passed in. We'll set pool.lst_mint to it.
+    #[account(mut)]
+    pub lst_mint: Account<'info, Mint>,
+
+    // Pool PDA - created off-chain with seeds ["pool"].
+    #[account(
+        init,
+        payer = admin,
+        space = 8 + Pool::INIT_SPACE,
+        seeds = [b"pool"],
+        bump
+    )]
+    pub pool: Account<'info, Pool>,
+
+    // Reserve PDA: program-derived system account to hold lamports for liquidity
+    // We'll create it as a system account with seeds ["pool_reserve"]
+    #[account(
+        init,
+        payer = admin,
+        space = 8,
+        seeds = [b"pool_reserve"],
+        bump
+    )]
+    pub reserve: AccountInfo<'info>,
+
+    // This is the signer PDA used to mint tokens (pool PDA)
+    // CHECK: we use pool as signer for the mint CPI
+    // In CPI we pass pool_signer as authority
+    #[account(seeds = [b"pool"], bump)]
+    pub pool_signer: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>
 }
--- programs/stakely/src/states.rs
+++ programs/stakely/src/states.rs
@@ -1,1 +1,7 @@
 use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct Pool {
+
+}
