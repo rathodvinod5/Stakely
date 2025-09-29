48a694e30fe1d6ffed0e18a5c75f666c53e4ed19
rathodvinod5 <rathodvinod5@gmail.com>
2025-09-29 15:07:37 +0530

    added account struct for request unstake

--- programs/stakely/src/instructions/process_unstake.rs
+++ programs/stakely/src/instructions/process_unstake.rs
@@ -1,4 +1,7 @@
 use anchor_lang::prelude::*;
+
+use crate::states::{ Pool, UnstakeTicket };
+use crate::errors::{ CustomErrors };
   
 // Admin function: process an UnstakeTicket (transfer lamports when available)
 // - The admin or keeper will call this when sufficient liquid lamports exist in Reserve
@@ -7,6 +10,28 @@ pub fn process_unstake(ctx: Context<ProcessUnstake>) -> Result<()> {
 }
 
 #[derive(Accounts)]
-pub struct ProcessUnstake {
+pub struct ProcessUnstake<'info> {
+    #[account(mut)]
+    pub requester: AccountInfo<'info>,
 
+    #[account(mut, has_one = reserve)]
+    pub pool: Account<'info, Pool>,
+
+    #[account(
+        mut,
+        seeds = [b"pool_reserve"],
+        bump
+    )]
+    pub reserve: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        close = requester,
+        constraint = !unstake_ticket.released @ CustomErrors::FundAlreadyReleased,
+        seeds = [b"unstake_ticket", pool.key().as_ref(), &pool.unstaked_count.to_le_bytes()],
+        bump
+    )]
+    pub unstake_ticket: Account<'info, UnstakeTicket>,
+
+    pub system_program: Program<'info, System>
 }
