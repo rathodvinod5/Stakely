262153a6404786a992a5ffca3b8be718f2d4d5d4
rathodvinod5 <rathodvinod5@gmail.com>
2025-09-29 16:24:41 +0530

    added accounts struct to mock accrue rewards

--- programs/stakely/src/instructions/mock_accrue_rewards.rs
+++ programs/stakely/src/instructions/mock_accrue_rewards.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 
+use crate::states::{ Pool };
 // Admin/keeper: mock_accrue_rewards
 // - The admin first transfers lamports to the pool reserve (off-chain client does SystemProgram::transfer to PDA)
 // - Then calls this instruction with amount to account for those lamports as "rewards" (adds to total_staked)
@@ -8,6 +9,15 @@ pub fn mock_accrue_rewards(ctx: Context<MockAccrueRewards>) -> Result<()> {
 }
 
 #[derive(Accounts)]
-pub struct MockAccrueRewards {
+pub struct MockAccrueRewards<'info> {
+    #[account(mut, signer)]
+    pub admin: AccountInfo<'info>,
 
+    #[account(mut, has_one = reserve)]
+    pub pool: Account<'info, Pool>,
+
+    #[account(mut, seeds = [b"pool_reserve"], bump)]
+    pub reserve: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>
 }
