c101e8e5543d2d5ecbcb2c901c5903de0455257f
rathodvinod5 <rathodvinod5@gmail.com>
2025-09-29 16:00:51 +0530

    added instruction set for process unstake ticket

--- programs/stakely/src/instructions/process_unstake.rs
+++ programs/stakely/src/instructions/process_unstake.rs
@@ -1,4 +1,7 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::transfer;
+use solana_program::program::invoke_signed;
+use solana_program::system_instruction;
 
 use crate::states::{ Pool, UnstakeTicket };
 use crate::errors::{ CustomErrors };
@@ -6,6 +9,45 @@ use crate::errors::{ CustomErrors };
 // Admin function: process an UnstakeTicket (transfer lamports when available)
 // - The admin or keeper will call this when sufficient liquid lamports exist in Reserve
 pub fn process_unstake(ctx: Context<ProcessUnstake>) -> Result<()> {
+    let pool = &ctx.accounts.pool;
+    let unstake_ticket = &mut ctx.accounts.unstake_ticket;
+   
+    require!(!unstake_ticket.released, CustomErrors::FundAlreadyReleased);
+
+    // let reserve_lamports = ctx.accounts.reserve.get_lamports();
+    let reserve_lamports = **ctx.accounts.reserve.to_account_info().lamports.borrow();
+    // let requested_lamports = unstake_ticket.requested_lamports
+    //     .try_into()
+    //     .map_err(|_| error!(CustomErrors::MathOverflow))?;
+    let requested_lamports = unstake_ticket.requested_lamports
+            .try_into()
+            .map_err(|_| error!(CustomErrors::MathOverflow))?;
+
+    require!(requested_lamports >= reserve_lamports, CustomErrors::InsufficientBalance);
+
+    // Transfer lamports from reserve PDA to user
+    // Since reserve is owned by system program and PDA controlled by program, use invoke_signed with reserve seeds
+    let seeds = [b"pool".as_ref(), &[pool.bump]];
+    let signers_seeds = &[&seeds[..]];
+    let system_program = &ctx.accounts.system_program;
+
+    let instructions = system_instruction::transfer(
+        &ctx.accounts.reserve.key(), 
+        &unstake_ticket.requester.key(), 
+        requested_lamports
+    );
+    let _ = invoke_signed(
+        &instructions, 
+        &[
+            ctx.accounts.reserve.to_account_info(),
+            ctx.accounts.requester.to_account_info(),
+            system_program.to_account_info()
+        ], 
+        signers_seeds
+    );
+
+    unstake_ticket.released = true;
+
     Ok(())
 }
 
