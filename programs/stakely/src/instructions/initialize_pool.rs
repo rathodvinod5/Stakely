da93bb8fe4ddf38ffa0e7bf940102a7d248d8276
rathodvinod5 <rathodvinod5@gmail.com>
2025-11-22 19:44:32 +0530

    fixed imports and ran anchor build to test all the code is valid or not

--- programs/stakely/src/instructions/initialize_pool.rs
+++ programs/stakely/src/instructions/initialize_pool.rs
@@ -1,65 +1,72 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token:: { Mint, Token };
+use anchor_spl::token::{Mint, Token};
+// use anchor_spl::associated_token::AssociatedToken;
+// use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
-use crate::states::{ Pool };
+
+use crate::states::Pool;
 
 // Initialize the pool:
 // - Creates Pool account (PDA)
 // - Creates LST mint (SPL) with pool as mint authority (PDA)
 // - Creates reserve PDA (System account) to hold liquid lamports
 pub fn initialize_pool(ctx: Context<InitializePool>, lst_decimals: u8) -> Result<()> {
-    // Initialize the mint to be controlled by pool PDA
-    // (We use CPI outside - the Anchor macro already ensured mint account exists).
     let pool = &mut ctx.accounts.pool;
     let decimals = ctx.accounts.lst_mint.decimals;
 
     pool.admin = ctx.accounts.admin.key();
     pool.lst_mint = ctx.accounts.lst_mint.key();
     pool.reserve = ctx.accounts.reserve.key();
-    pool.bump = ctx.bumps.pool;
-    // pool.lst_decimals = lst_decimals;
     pool.lst_decimals = decimals;
-    pool.total_staked = 0u128;
-    pool.total_lst_minted = 0u128;
+    pool.total_staked = 0 as u128;
+    pool.total_lst_minted = 0 as u128;
     pool.staked_count = 0;
     pool.unstaked_count = 0;
+    pool.bump = ctx.bumps.pool;
 
     Ok(())
 }
 
 #[derive(Accounts)]
 pub struct InitializePool<'info> {
+
+    /// CHECK: This is the admin initializing the pool
     #[account(mut, signer)]
     pub admin: AccountInfo<'info>,
 
+    /// CHECK: This is the LST mint account
     #[account(mut)]
     pub lst_mint: Account<'info, Mint>,
 
+    /// CHECK: This is the pool account
     #[account(
         init,
         payer = admin,
         space = 8 + Pool::INIT_SPACE,
-        seeds = [b"pool"],
+        // seeds = [b"pool", admin.key().as_ref(), lst_mint.key().as_ref()],
+        seeds = [b"pool", lst_mint.key().as_ref()],
         bump
     )]
     pub pool: Account<'info, Pool>,
 
+    /// CHECK: This is a PDA vault account used only to hold SOL. No data is accessed.
     #[account(
         init,
         payer = admin,
         space = 8,
-        seeds = [b"pool_reserve"],
+        seeds = [],
         bump
     )]
-    pub reserve: AccountInfo<'info>,
-
-    #[account(
-        seeds = [b"pool"],
-        bump
-    )]
-    pub pool_signer: AccountInfo<'info>,
+    pub reserve: UncheckedAccount<'info>,
 
     pub system_program: Program<'info, System>,
-    pub token_account: Program<'info, Token>,
-    pub rent: Sysvar<'info, Rent>
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+
+    // pub token_account: InterfaceAccount<'info, TokenAccount>,
+    // pub lst_mint: InterfaceAccount<'info, Mint>,
+    // pub token_program: Interface<'info, TokenInterface>,
+    // // pub associated_token_program: Program<'info, AssociatedToken>,
+    // pub system_program: Program<'info, System>,
+    // pub rent: Sysvar<'info, Rent>,
 }
