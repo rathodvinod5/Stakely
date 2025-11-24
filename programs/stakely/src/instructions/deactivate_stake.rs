4221ecc7ff50ec978c47a678057151eff0030103
rathodvinod5 <rathodvinod5@gmail.com>
2025-11-25 00:30:52 +0530

    recovered code from comments

--- programs/stakely/src/instructions/deactivate_stake.rs
+++ programs/stakely/src/instructions/deactivate_stake.rs
@@ -1,63 +1,63 @@
-// use anchor_lang::prelude::*;
-// use solana_program::{ 
-//     stake::instruction::deactivate_stake,
-//     program::invoke_signed,
-// };
+use anchor_lang::prelude::*;
+use solana_program::{ 
+    stake::instruction::deactivate_stake,
+    program::invoke_signed,
+};
 
-// use crate::states::{ Pool, StakeEntry, StakeStatus };
-// use crate::errors::CustomErrors;
+use crate::states::{ Pool, StakeEntry, StakeStatus };
+use crate::errors::CustomErrors;
 
-// // Admin: deactivate a stake account and withdraw its lamports back to reserve (keeper operation)
-// // - This will call stake::deactivate_stake then after deactivation and when lamports released, withdraw to reserve
-// pub fn deactivate_stake_account(ctx: Context<DeactivateStakeAccount>) -> Result<()> {
-//     let pool = &mut ctx.accounts.pool;
-//     let stake_entry = &mut ctx.accounts.stake_entry;
-//     let stake_account = &ctx.accounts.stake_account;
+// Admin: deactivate a stake account and withdraw its lamports back to reserve (keeper operation)
+// - This will call stake::deactivate_stake then after deactivation and when lamports released, withdraw to reserve
+pub fn deactivate_stake_account(ctx: Context<DeactivateStakeAccount>) -> Result<()> {
+    let pool = &mut ctx.accounts.pool;
+    let stake_entry = &mut ctx.accounts.stake_entry;
+    let stake_account = &ctx.accounts.stake_account;
 
-//     require!(stake_entry.pool == pool.key(), CustomErrors::KeyMismatch);
+    require!(stake_entry.pool == pool.key(), CustomErrors::KeyMismatch);
 
-//     let deactivate_ix = deactivate_stake(
-//         &stake_account.key(), 
-//         &pool.key()
-//     );
-//     let seeds = &[b"pool".as_ref(), &[pool.bump]];
-//     let signer_seeds = &[&seeds[..]];
+    let deactivate_ix = deactivate_stake(
+        &stake_account.key(), 
+        &pool.key()
+    );
+    let seeds = &[b"pool".as_ref(), &[pool.bump]];
+    let signer_seeds = &[&seeds[..]];
 
-//     let result = invoke_signed(
-//         &deactivate_ix, 
-//         &[
-//             stake_account.to_account_info(),
-//             ctx.accounts.clock.to_account_info(),
-//             ctx.accounts.stake_program.to_account_info(),
-//         ], 
-//         signer_seeds
-//     );
+    let result = invoke_signed(
+        &deactivate_ix, 
+        &[
+            stake_account.to_account_info(),
+            ctx.accounts.clock.to_account_info(),
+            ctx.accounts.stake_program.to_account_info(),
+        ], 
+        signer_seeds
+    );
 
-//     if !pool.deactivating_stakes.contains(&stake_account.key()) {
-//         pool.deactivating_stakes.push(stake_account.key());
-//     }
+    if !pool.deactivating_stakes.contains(&stake_account.key()) {
+        pool.deactivating_stakes.push(stake_account.key());
+    }
 
-//     stake_entry.status = StakeStatus::Deactivating;
+    stake_entry.status = StakeStatus::Deactivating;
 
-//     Ok(())
-// }
+    Ok(())
+}
 
-// #[derive(Accounts)]
-// pub struct DeactivateStakeAccount<'info> {
-//     #[account(mut)]
-//     pub admin: Signer<'info>,
+#[derive(Accounts)]
+pub struct DeactivateStakeAccount<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
 
-//     #[account(mut, has_one = admin)]
-//     pub pool: Account<'info, Pool>,
+    #[account(mut, has_one = admin)]
+    pub pool: Account<'info, Pool>,
 
-//     #[account(mut)]
-//     pub stake_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub stake_account: AccountInfo<'info>,
 
-//     #[account(mut, has_one = pool.key())]
-//     pub stake_entry: Account<'info, StakeEntry>,
+    #[account(mut, has_one = pool.key())]
+    pub stake_entry: Account<'info, StakeEntry>,
 
-//     #[account(address = solana_program::stake::program::ID)]
-//     pub stake_program: AccountInfo<'info>,
+    #[account(address = solana_program::stake::program::ID)]
+    pub stake_program: AccountInfo<'info>,
 
-//     pub clock: Sysvar<'info, Clock>
-// }
+    pub clock: Sysvar<'info, Clock>
+}
